a404fcbca5a08b2486ea00670f877ac4ef228896
barnden <brandon@nguyen.vc>
2022-08-17 17:03:45 -0500

    Add XOR skeleton code

---
+++ A1/XOR.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        inp = torch.zeros([4, 2], dtype=torch.int32)
+        tgt = 
+
+    def forward(self, x):
+        return x
+
+### Create an instance of the Net class
+net = Net()
+
+
+
+### Define the input and the target
+
+
+
+### Define the loss and create your optimizer
+
+
+### Main training loop
+for i in range(10000):
+    
+
+
+	
+### Testing the network
