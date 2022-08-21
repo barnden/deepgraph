99c7ddf4864a187a78904a94cb9c92d9843c5e81
barnden <brandon@nguyen.vc>
2022-08-21 14:32:53 -0500

    Update SuperResolution

--- SuperResolution/RGB.py
+++ SuperResolution/RGB.py
@@ -8,20 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-class SkipConnection(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
-        super().__init__()
-
-        self.skip = nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
-        )
-
-    def forward(self, x):
-        return self.skip(x)
-
-
 class Net(nn.Module):
     def __init__(self, in_channels=3):
         super(Net, self).__init__()
@@ -43,21 +29,14 @@ class Net(nn.Module):
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1),
             nn.ReLU(),
-            SkipConnection(64, 32, 4, stride=2, padding=1),
+
+            nn.ConvTranspose2d(64, 16, 4, stride=2, padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(32, 16, 4, stride=2, padding=1),
-            nn.ReLU(),
-            SkipConnection(32, 16, 4, stride=2, padding=1),
+            nn.ConvTranspose2d(32, 8, 4, stride=2, padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(16, 8, 4, stride=2, padding=1),
-            nn.ReLU(),
-            SkipConnection(16, 8, 4, stride=2, padding=1),
-            nn.ReLU(),
-
-            nn.ConvTranspose2d(8, in_channels, 4, stride=2, padding=1),
-            nn.ConvTranspose2d(in_channels, in_channels, 4, stride=2, padding=1),
+            nn.ConvTranspose2d(16, in_channels, 4, stride=2, padding=1),
             nn.Sigmoid()
         )
 
@@ -84,9 +63,10 @@ class Net(nn.Module):
             self.encoder[1].register_forward_hook(save_activation_map_as('conv_1')),
             self.encoder[3].register_forward_hook(save_activation_map_as('conv_2')),
             self.encoder[5].register_forward_hook(save_activation_map_as('conv_3')),
+
             self.decoder[1].register_forward_hook(concat_activation_with('conv_3')),
-            self.decoder[5].register_forward_hook(concat_activation_with('conv_2')),
-            self.decoder[9].register_forward_hook(concat_activation_with('conv_1')),
+            self.decoder[3].register_forward_hook(concat_activation_with('conv_2')),
+            self.decoder[5].register_forward_hook(concat_activation_with('conv_1')),
         ]
 
     def forward(self, x):
@@ -116,20 +96,21 @@ for X, y in testloader:
     break
 
 loss_fn = nn.MSELoss()
-optimizer = optim.Adam(net.parameters())
+optimizer = optim.Adam(net.parameters(), lr=1e-3)
 
 size = len(trainloader.dataset)
 
 # Set the below to False to skip training and use saved model "RGB_model.pth"
 if True:
-    for epoch in range(6):
+    for epoch in range(5):
         print('-' * 32)
         print(f"Epoch {epoch + 1}")
         print('-' * 32)
 
         for batch, (X, _) in enumerate(trainloader, 1):
             y = X.to(device)
-            X = F.interpolate(X, (48, 48), mode="bilinear").to(device)
+            X = F.interpolate(X, (48, 48), mode="bilinear")
+            X = F.interpolate(X, (96, 96), mode="bilinear").to(device)
 
             pred = net(X)
             loss = loss_fn(pred, y)
@@ -140,7 +121,7 @@ if True:
 
             if batch % 1000 == 0:
                 loss, current = loss.item(), batch * len(X)
-                print(f"loss: {loss:.7e} [{current:>5d}/{size:>5d}]")
+                print(f"loss: {loss:.7e} [{current:>6d}/{size:>6d}]")
 
     torch.save(net.state_dict(), "RGB_model.pth")
 else:
@@ -150,7 +131,8 @@ test_loss = 0
 with torch.no_grad():
     for (X, _) in testloader:
         y = X.to(device)
-        X = F.interpolate(X, (48, 48), mode="bilinear").to(device)
+        X = F.interpolate(X, (48, 48), mode="bilinear")
+        X = F.interpolate(X, (96, 96), mode="bilinear").to(device)
 
         pred = net(X)
         test_loss += loss_fn(pred, y).item()
@@ -162,16 +144,18 @@ print(f"Test Loss: {test_loss:>8f}\n")
 with torch.no_grad():
     test_images = []
     out_images = []
+    target_images = []
 
     for i in range(5):
         x, _ = testset[i]
 
+        target_images.append(x.cpu())
+
         X = F.interpolate(x[None], (48, 48), mode="bilinear")
+        X = F.interpolate(X, (96, 96), mode="bilinear")
 
         test_img = torch.squeeze(X)
-
-        test_img_pad = F.pad(test_img, (24, 24, 24, 24), "constant", 0)
-        test_images.append(test_img_pad)
+        test_images.append(test_img)
 
         plt.imsave(f"./SR/input_{i}.png", test_img.permute(1, 2, 0).numpy())
 
@@ -183,7 +167,7 @@ with torch.no_grad():
 
         plt.imsave(f"./SR/output_{i}.png", pred_img.permute(1, 2, 0).numpy())
 
-    images = test_images + out_images
+    images = test_images + out_images + target_images
 
     grid = torchvision.utils.make_grid(images, nrow=5).permute(1, 2, 0).numpy()
     plt.imsave(f"./SR/result_grid.png", grid)
